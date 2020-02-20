3f714eaabe727c15d777fa04d58be9575493d2e5
Jorge Prado Fernandez <jprado@f-l3210-pc25.aulas.etsit.urjc.es>
2020-02-20 10:49:14 +0100

    diseño calculadora

---
+++ P2/index.js
@@ -0,0 +1,75 @@
+console.log("Ejecutando js...");
+
+const test0 = document.getElementById('test0')
+const test1 = document.getElementById('test1')
+const test2 = document.getElementById('test2')
+const test3 = document.getElementById('test3')
+const test4 = document.getElementById('test4')
+const test5 = document.getElementById('test5')
+const test6 = document.getElementById('test6')
+const test7 = document.getElementById('test7')
+const test8 = document.getElementById('test8')
+const test9 = document.getElementById('test9')
+const testpoint = document.getElementById('testpoint')
+const testequal = document.getElementById('testequal')
+const testAC = document.getElementById('testAC')
+const testborrar = document.getElementById('testborrar')
+const testmult = document.getElementById('testmult')
+const testdiv = document.getElementById('testdiv')
+const testsuma = document.getElementById('testsuma')
+const testresta = document.getElementById('testresta')
+
+test0.onclick = () => {
+  console.log("0");
+  }
+test1.onclick = () => {
+  console.log("1");
+  }
+test2.onclick = () => {
+  console.log("2");
+  }
+test3.onclick = () => {
+  console.log("3");
+  }
+test4.onclick = () => {
+  console.log("4");
+  }
+test5.onclick = () => {
+  console.log("5");
+  }
+test6.onclick = () => {
+  console.log("6");
+  }
+test7.onclick = () => {
+  console.log("7");
+  }
+test8.onclick = () => {
+  console.log("8");
+  }
+test9.onclick = () => {
+  console.log("9");
+  }
+testpoint.onclick = () => {
+  console.log(".");
+  }
+testequal.onclick = () => {
+  console.log("=");
+  }
+testAC.onclick = () => {
+  console.log("0");
+  }
+testborrar.onclick = () => {
+  console.log("Yo borraré");
+  }
+testmult.onclick = () => {
+  console.log("x");
+  }
+testdiv.onclick = () => {
+  console.log("%");
+  }
+testsuma.onclick = () => {
+  console.log("+");
+  }
+testresta.onclick = () => {
+  console.log("-");
+  }
