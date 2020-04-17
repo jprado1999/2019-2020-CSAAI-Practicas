029ac0be32b08b7ab718bf309923bae95b1a8ce6
pc <pc@ubuntu.ubuntu-domain>
2020-04-17 20:33:38 +0200

    Seguimos con la p4

--- P4/index.js
+++ P4/index.js
@@ -16,6 +16,7 @@ video2.height=100;
 const video3 = document.getElementById("video3")
 video3.width=200;  //-- Tamaño de la pantalla de video
 video3.height=100;
+
 //-- Imagen estática a mostrar cuando el video no
 //-- ha arrancado
 video_master.poster="https://github.com/myTeachingURJC/2019-2020-CSAAI/raw/master/L10/test.png";
@@ -33,54 +34,72 @@ const stop2 = document.getElementById("stop2")
 
 const play3 = document.getElementById("play3")
 const stop3 = document.getElementById("stop3")
-//-- Función de retrollamada del botón de ver
 
+video1.src="https://gsyc.urjc.es/jmplaza/csaai/realizador-fuente1.mp4"
+video1.play();
+video1.muted = true;
+
+video2.src="https://gsyc.urjc.es/jmplaza/csaai/realizador-fuente2.mp4"
+video2.play();
+video2.muted = true;
+
+video3.src="https://gsyc.urjc.es/jmplaza/csaai/realizador-fuente3.mp4"
+video3.play();
+video3.muted = true;
+
+//-- Función de retrollamada del botón de ver
 play.onclick = () => {
   console.log("Click!");
-  video1.src="https://gsyc.urjc.es/jmplaza/csaai/realizador-fuente1.mp4"
-  video1.play();
   video_master.src="https://gsyc.urjc.es/jmplaza/csaai/realizador-fuente1.mp4"
   video_master.play();
-};
 
-play2.onclick = () => {
-  console.log("Click!");
-  video2.src="https://gsyc.urjc.es/jmplaza/csaai/realizador-fuente2.mp4"
-  video2.play();
-  video_master.src="https://gsyc.urjc.es/jmplaza/csaai/realizador-fuente2.mp4"
-  video_master.play();
-};
-
-play3.onclick = () => {
-  console.log("Click!");
-  video3.src="https://gsyc.urjc.es/jmplaza/csaai/realizador-fuente3.mp4"
-  video3.play();
-  video_master.src="https://gsyc.urjc.es/jmplaza/csaai/realizador-fuente3.mp4"
-  video_master.play();
-};
-//-- Funcion de retrollamada del boton de parar
-stop.onclick = () => {
   video1.pause();
 
   //-- Quitar la fuente de video, para que se muestre la
   //-- imagen definida en el atributo poster
-  video1.src=null;
+  video1.src="https://github.com/myTeachingURJC/2019-2020-CSAAI/raw/master/L10/test.png";;
+};
+
+play2.onclick = () => {
+  console.log("Click!");
+  video_master.src="https://gsyc.urjc.es/jmplaza/csaai/realizador-fuente2.mp4"
+  video_master.play();
+
+  video2.pause();
+  video2.src="https://github.com/myTeachingURJC/2019-2020-CSAAI/raw/master/L10/test.png";;
+};
+
+play3.onclick = () => {
+  console.log("Click!");
+  video_master.src="https://gsyc.urjc.es/jmplaza/csaai/realizador-fuente3.mp4"
+  video_master.play();
+
+  video3.pause();
+  video3.src="https://github.com/myTeachingURJC/2019-2020-CSAAI/raw/master/L10/test.png";;
+};
+//-- Funcion de retrollamada del boton de parar
+stop.onclick = () => {
   video_master.pause();
-  video_master.src=null;
+  video_master.src="https://github.com/myTeachingURJC/2019-2020-CSAAI/raw/master/L10/test.png";;
+  video1.src="https://gsyc.urjc.es/jmplaza/csaai/realizador-fuente1.mp4"
+  video1.play();
+  video1.muted = true;
 }
 
 stop2.onclick = () => {
-  video2.pause();
-  video2.src=null;
   video_master.pause();
-  video_master.src=null;
+  video_master.src="https://github.com/myTeachingURJC/2019-2020-CSAAI/raw/master/L10/test.png";;
+  video2.src="https://gsyc.urjc.es/jmplaza/csaai/realizador-fuente2.mp4"
+  video2.play();
+  video2.muted = true;
 }
 
 stop3.onclick = () => {
-  video3.pause();
-  video3.src=null;
   video_master.pause();
-  video_master.src=null;
+  video_master.src="https://github.com/myTeachingURJC/2019-2020-CSAAI/raw/master/L10/test.png";;
+  video3.src="https://gsyc.urjc.es/jmplaza/csaai/realizador-fuente3.mp4"
+  video3.play();
+  video3.muted = true;
 }
 /*Meter los comandos para que de base se esten reproduciendo los tres videos en la parte de abajo
 video1.src="https://gsyc.urjc.es/jmplaza/csaai/realizador-fuente1.mp4"
