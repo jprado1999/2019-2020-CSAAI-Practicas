5cf00824b5d35d5ec540b4ca04af9bed43b95d05
pc <pc@ubuntu.ubuntu-domain>
2020-04-21 10:41:14 +0200

    Clase de la p5

---
+++ P5/deslizador.js
@@ -0,0 +1,6 @@
+const deslizador = document.getElementById('deslizador')
+const display = document.getElementById('display')
+
+deslizador.oninput = () => {
+    display.innerHTML = deslizador.value
+}
