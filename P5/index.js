1bba33f51d07a8b08516ab0e2df1030d9a3940fa
pc <pc@ubuntu.ubuntu-domain>
2020-05-15 19:44:34 +0200

    Seguimos con la P5

--- P5/index.js
+++ P5/index.js
@@ -1,20 +1,27 @@
-console.log("Ejecutando JS....")
+console.log("Ejecutando JS....");
 
 //-- Obtener elementos del DOM
 const canvas = document.getElementById('canvas');
 const img = document.getElementById('imagesrc');
 const ctx = canvas.getContext('2d');
+let imgData;
+let data;
 
 //-- Acceso al deslizador
-const deslizador = document.getElementById('deslizador');
+const deslizador_R = document.getElementById('deslizador_R');
+const deslizador_G = document.getElementById('deslizador_G');
+const deslizador_B = document.getElementById('deslizador_B');
 
 //-- Valor del deslizador
-const range_value = document.getElementById('range_value');
+const range_value_R = document.getElementById('range_value_R');
+const range_value_G = document.getElementById('range_value_G');
+const range_value_B = document.getElementById('range_value_B');
 
 //-- Función de retrollamada de imagen cargada
 //-- La imagen no se carga instantaneamente, sino que
 //-- lleva un tiempo. Sólo podemos acceder a ella una vez
 //-- que esté totalmente cargada
+
 img.onload = function () {
 
   //-- Se establece como tamaño del canvas el mismo
@@ -24,36 +31,85 @@ img.onload = function () {
 
   //-- Situar la imagen original en el canvas
   //-- No se han hecho manipulaciones todavia
-  ctx.drawImage(img, 0,0);
+  ctx.drawImage(img, 0, 0);
 
   console.log("Imagen lista...");
 };
 
-
 //-- Funcion de retrollamada del deslizador
-deslizador.oninput = () => {
+deslizador_R.oninput = () => {
   //-- Mostrar el nuevo valor del deslizador
-  range_value.innerHTML = deslizador.value;
+  range_value_R.innerHTML = deslizador_R.value;
 
   //-- Situar la imagen original en el canvas
   //-- No se han hecho manipulaciones todavia
-  ctx.drawImage(img, 0,0);
+  //ctx.drawImage(img, 0,0);
 
   //-- Obtener la imagen del canvas en pixeles
-  let imgData = ctx.getImageData(0, 0, canvas.width, canvas.height);
+  imgData = ctx.getImageData(0, 0, canvas.width, canvas.height);
 
   //-- Obtener el array con todos los píxeles
-  let data = imgData.data
+  data = imgData.data;
 
-  //-- Obtener el umbral de rojo del desliador
-  umbral = deslizador.value
+  //-- Obtener el umbral de rojo del deslizador
+  umbral_R = deslizador_R.value
 
   //-- Filtrar la imagen según el nuevo umbral
   for (let i = 0; i < data.length; i+=4) {
-    if (data[i] > umbral)
-      data[i] = umbral;
+    if (data[i] > umbral_R)
+      data[i] = umbral_R;
   }
+  //-- Poner la imagen modificada en el canvas
+  ctx.putImageData(imgData, 0, 0);
+}
 
+deslizador_G.oninput = () => {
+  //-- Mostrar el nuevo valor del deslizador
+  range_value_G.innerHTML = deslizador_G.value;
+
+  //-- Situar la imagen original en el canvas
+  //-- No se han hecho manipulaciones todavia
+  //ctx.drawImage(img, 0,0);
+
+  //-- Obtener la imagen del canvas en pixeles
+  imgData = ctx.getImageData(0, 0, canvas.width, canvas.height);
+
+  //-- Obtener el array con todos los píxeles
+  data = imgData.data;
+
+  //-- Obtener el umbral de verde del desliador
+  umbral_G = deslizador_G.value
+
+  //-- Filtrar la imagen según el nuevo umbral
+  for (let i = 1; i < data.length; i+=4) {
+    if (data[i] > umbral_G)
+      data[i] = umbral_G;
+  }
+  //-- Poner la imagen modificada en el canvas
+  ctx.putImageData(imgData, 0, 0);
+}
+deslizador_B.oninput = () => {
+  //-- Mostrar el nuevo valor del deslizador
+  range_value_B.innerHTML = deslizador_B.value;
+
+  //-- Situar la imagen original en el canvas
+  //-- No se han hecho manipulaciones todavia
+  //ctx.drawImage(img, 0,0);
+
+  //-- Obtener la imagen del canvas en pixeles
+  imgData = ctx.getImageData(0, 0, canvas.width, canvas.height);
+
+  //-- Obtener el array con todos los píxeles
+  data = imgData.data;
+
+  //-- Obtener el umbral de rojo del desliador
+  umbral_B = deslizador_B.value
+
+  //-- Filtrar la imagen según el nuevo umbral
+  for (let i = 2; i < data.length; i+=4) {
+    if (data[i] > umbral_B)
+      data[i] = umbral_B;
+  }
   //-- Poner la imagen modificada en el canvas
   ctx.putImageData(imgData, 0, 0);
 }
